40040fd9b72a4cdc72c4db87c1243fa4d1d24e52
William M. Tsikata <tsikatawill@gmail.com>
2023-01-01 21:58:05 +0000

    chore: hide scrollbar

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -11,7 +11,7 @@ export const Sidebar = ({ handleClose }) => {
       animate="open"
       exit="closed"
       variants={sidebarAnim}
-      className="fixed top-20 flex min-h-[calc(100vh-5rem)] w-52 flex-col items-center gap-5 overflow-hidden bg-gray-50 p-5"
+      className="scrollbar-hidden fixed top-20 flex h-[calc(100vh-5rem)] w-52 flex-col items-center gap-5 overflow-x-hidden overflow-y-scroll bg-gray-50 p-5"
       onClick={handleClose}
     >
       {Routes.map((route, idx) => (
