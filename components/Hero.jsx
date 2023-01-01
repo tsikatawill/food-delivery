b25fe78c515fe063962e35fc75bca42f6a19f099
William M. Tsikata <tsikatawill@gmail.com>
2023-01-01 23:27:49 +0000

    feat: add hero section

---
+++ components/Hero.jsx
@@ -0,0 +1,49 @@
+import { Button, Container } from "../components";
+import { headerTop, popUp, slideY } from "../animations";
+
+import { motion } from "framer-motion";
+
+export const Hero = () => {
+  return (
+    <section>
+      <div className="top h-fit py-10 sm:h-[600px] sm:bg-[url(/images/bg-hero.jpg)]">
+        <Container className="h-full">
+          <motion.div
+            variants={headerTop}
+            initial="initial"
+            whileInView="animate"
+            className="flex h-full flex-col items-center gap-5 text-center"
+          >
+            <motion.p
+              variants={slideY()}
+              className="text-xl font-[700]  sm:text-2xl"
+            >
+              Food app
+            </motion.p>
+            <motion.p
+              variants={popUp}
+              className="max-w-4xl text-5xl font-[700] leading-[60px] sm:text-6xl sm:leading-[80px]"
+            >
+              Why stay hungry when you can order from Bella Onojie .{" "}
+            </motion.p>
+            <motion.p className="text-[24px]" variants={slideY("bottom")}>
+              Download the Bella Onojie{"'"}s food app now on
+            </motion.p>
+
+            <motion.div
+              className="mt-5 flex flex-col gap-4 sm:flex-row"
+              variants={slideY("bottom")}
+            >
+              <Button>Playstore</Button>
+              <Button bg="transparent">Appstore</Button>
+            </motion.div>
+          </motion.div>
+        </Container>
+      </div>
+
+      <Container>
+        <div className="py-10"></div>
+      </Container>
+    </section>
+  );
+};
--- components/index.js
+++ components/index.js
@@ -1,5 +1,7 @@
+export * from "./Button";
 export * from "./Container";
 export * from "./Footer";
+export * from "./Hero";
 export * from "./Layout";
 export * from "./Navbar";
 export * from "./Sidebar";
