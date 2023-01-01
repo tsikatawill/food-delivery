edba067c837bd88f8b9206154d96e4b4397e54e8
William M. Tsikata <tsikatawill@gmail.com>
2023-01-01 21:19:10 +0000

    chore: add id attribute

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import { Container } from "../components";
 
 export const Footer = () => {
   return (
-    <footer className="flex-grow-0 flex-shrink-0">
+    <footer className="flex-shrink-0 flex-grow-0" id="contact">
       <Container>Footer</Container>
     </footer>
   );
