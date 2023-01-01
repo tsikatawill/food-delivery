aafad1a5860f24aeee43bf044d67aea7c3954712
William M. Tsikata <tsikatawill@gmail.com>
2023-01-01 21:40:05 +0000

    chore: handle active nav link state

--- components/Navbar/NavLink.jsx
+++ components/Navbar/NavLink.jsx
@@ -1,10 +1,19 @@
 import Link from "next/link";
 import { motion } from "framer-motion";
+import { useRouter } from "next/router";
 
 export const NavLink = ({ name, path, variants, ...rest }) => {
+  const router = useRouter();
+
   return (
     <Link href={path} passHref legacyBehavior>
-      <motion.a variants={variants} {...rest} className="w-full">
+      <motion.a
+        variants={variants}
+        {...rest}
+        className={`w-full ${
+          router.asPath === path ? "text-orange-500" : "text-black"
+        }`}
+      >
         {name}
       </motion.a>
     </Link>
