8cea3eb2fa994676313d610088db598f5cd182ad
William M. Tsikata <tsikatawill@gmail.com>
2023-01-03 21:58:50 +0000

    chore: modify site title + meta data

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,10 +2,10 @@ import { Footer, Navbar } from "../components";
 
 export const Layout = ({ children }) => {
   return (
-    <div className="flex min-h-screen flex-col">
+    <>
       <Navbar />
       <main className="flex-grow">{children}</main>
       <Footer />
-    </div>
+    </>
   );
 };
