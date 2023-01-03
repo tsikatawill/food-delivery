d38bbbdb1e7f773aaad2e3c6c0c2cb46ed5ea9cf
William M. Tsikata <tsikatawill@gmail.com>
2023-01-03 18:09:43 +0000

    refactor: change button props
    also add diferent styles for different devices

--- components/Button.jsx
+++ components/Button.jsx
@@ -4,24 +4,23 @@ import { motion } from "framer-motion";
 export const Button = ({
   children,
   bg = "primary",
-  rounded = "full",
+  rounded = false,
   ...rest
 }) => {
   const background =
     bg === "primary"
-      ? "bg-orange-500 text-white hover:bg-orange-600"
-      : "bg-none border-white hover:bg-white hover:text-orange-500";
-  const roundness =
-    rounded === "full"
-      ? "rounded-l-full rounded-r-full"
-      : "rounded-l-lg rounded-r-lg";
+      ? "bg-orange-500 border-transparent text-white hover:bg-orange-600"
+      : "bg-none border-orange-500 hover:border-transparent sm:border-white text-orange-500 sm:text-white hover:bg-slate-100 hover:text-orange-500";
+  const roundness = rounded
+    ? "rounded-l-full rounded-r-full"
+    : "rounded-l-lg rounded-r-lg";
   return (
     <motion.button
       {...rest}
       variants={click}
       initial="initial"
       whileTap="animate"
-      className={`h-16 min-w-[230px] border border-transparent text-2xl font-bold transition-colors duration-150 ease-in ${background} ${roundness}`}
+      className={`h-16 min-w-[230px] border text-xl font-bold transition-colors duration-150 ease-in ${background} ${roundness}`}
     >
       {children}
     </motion.button>
